c7e2b5c98906bd589055ccdeec343fe8fbad7fa9
Karol Dzialowski <karlososhd@gmail.com>
2020-10-15 19:32:34 +0200

    feat: psnr calculator

---
+++ py_src/image_noiser.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import numpy as np
+import math
+
+def PSNR(original, compressed): 
+    mse = np.mean((original - compressed) ** 2) 
+    if(mse == 0):  # MSE is zero means no noise is present in the signal . 
+                  # Therefore PSNR have no importance. 
+        return 100
+    max_pixel = 255.0
+    psnr = 20 * math.log10(max_pixel / math.sqrt(mse)) 
+    return psnr 
+
+img = Image.open('../img/input/balloon.bmp')
+
+img_arr = np.array(img)
+img_arr_noise = img_arr & 254 
+
+img_noise = Image.fromarray(img_arr_noise)
+img_noise.save("../img/output/balloon_noise.bmp")
+
+print(f"PSNR: {PSNR(img_arr, img_arr_noise)}")
+
--- src/psnr.cpp
+++ src/psnr.cpp
@@ -5,19 +5,23 @@ Psnr::Psnr(){
 };
 
 double Psnr::calculate(Image &original, Image &reference) {
-  if (original.width != reference.width || original.height != reference.width) {
+  if (original.width != reference.width || original.height != reference.height) {
+    std::cout << "Psnr::calculate: image sizes are not the same" << std::endl;
+    std::cout << "Original: " << original.width << " " << original.height << std::endl;
+    std::cout << "Reference: " << reference.width << " " << reference.height << std::endl;
     return 0.0;
   }
 
-  double sum = 0.0;
+  double mse = 0.0;
   for (int i = 0; i < original.height; i++) {
-    for (int j = 0; j < original.width; i++) {
+    for (int j = 0; j < original.width; j++) {
       int diff = original.getPixel(i, j) - reference.getPixel(i, j);
-      sum += double(diff) * double(diff);
+      mse += double(diff) * double(diff);
     }
   }
+  mse = mse / (original.width * original.height);
 
-  double nominator = original.width * original.height * 255 * 255;
-  double result = std::log10(nominator/sum);
+  double nominator = 255 * 255;
+  double result = 10.0 * std::log10(nominator/mse);
   return result;
 }
